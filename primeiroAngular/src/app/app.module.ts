98f7bcced0ce6252aa94bd63a6b4c138f92bba1d
DeboraRodrigues1 <paulow.wesley33@gmail.com>
2019-12-13 16:39:48 -0300

    primeiro projeto com angular

---
+++ primeiroAngular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CorpoComponent } from './corpo/corpo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TituloComponent,
+    CabecalhoComponent,
+    CorpoComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
